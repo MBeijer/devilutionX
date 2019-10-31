d562030199096dbd11a2b83f1dc35f65c19b6027
Marlon Beijer <marlon.beijer@cybercom.com>
2019-10-31 17:39:24 +0100

    Try to fix the build on CircleCI

--- Packaging/amiga/prep.sh
+++ Packaging/amiga/prep.sh
@@ -6,8 +6,8 @@ set -e
 #set compiler params
 export TARGET='m68k-amigaos'
 export SYSROOT=/opt/$TARGET
-export FLAGS='-m68040 -mhard-float -s -ffast-math -O3 -w -fomit-frame-pointer  -noixemul -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14 -fbbb=abcdef'
-export DEFINES='-DM68K_CPU=68040 -DM68K_FPU=hard -DM68K_COMMON="-s -ffast-math -O3 -fno-exceptions -w -fomit-frame-pointer  -noixemul -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14 -fbbb=abcdef"'
+export FLAGS='-m68040 -mhard-float -s -ffast-math -O3 -w -fomit-frame-pointer  -noixemul -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14'
+export DEFINES='-DM68K_CPU=68040 -DM68K_FPU=hard -DM68K_COMMON="-s -ffast-math -O3 -fno-exceptions -w -fomit-frame-pointer  -noixemul -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"'
 
 # ZLIB
 wget https://www.zlib.net/zlib-1.2.11.tar.gz
@@ -42,7 +42,7 @@ wget https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.gz
 tar -xvf freetype-2.10.1.tar.gz
 mkdir -p freetype-2.10.1/build
 cd freetype-2.10.1/build
-eval cmake .. -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DUNIX=1 ${DEFINES}
+eval cmake .. -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DUNIX=1
 cmake --build . --config Release --target install -- -j$(nproc)
 cd ../..
 
@@ -51,6 +51,6 @@ wget https://github.com/SDL-mirror/SDL_ttf/archive/SDL-1.2.tar.gz -O SDL_ttf-SDL
 tar -xvf SDL_ttf-SDL-1.2.tar.gz
 cd SDL_ttf-SDL-1.2
 ./autogen.sh
-CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" SDL_LIBS='-lSDL -ldebug' SDL_CFLAGS="-I${SYSROOT}/include/SDL -noixemul" FT2_CFLAGS="-I${SYSROOT}/include/freetype2" FT2_LIBS="-lfreetype -lzlib" ./configure --disable-shared --enable-static --host=${TARGET} --prefix=${SYSROOT}
+SDL_LIBS='-lSDL -ldebug' SDL_CFLAGS="-I${SYSROOT}/include/SDL -noixemul" FT2_CFLAGS="-I${SYSROOT}/include/freetype2" FT2_LIBS="-lfreetype -lzlib" ./configure --disable-shared --enable-static --host=${TARGET} --prefix=${SYSROOT}
 make -j$(nproc)
 make install
